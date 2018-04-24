cdea52000611f377543ae5b8c7439ab8d2274357
guomin <yushi5344@gmail.com>
2018-04-24 22:34:09 +0800

    Vue中方法

--- js/app.js
+++ js/app.js
@@ -2,6 +2,17 @@
 new Vue({
     el:"#vue-app",
     data:{
-        name:"guomin"
+        name:"guomin",
+        job:"web开发"
+    },
+    methods:{
+        greet:function (time) {
+            return 'Good '+time+' '+this.name+'!';
+        }
     }
 });
+/*
+el:element 需要获取的元素，一定是html中的根容器元素
+data:用于数据的存储
+ */
+
