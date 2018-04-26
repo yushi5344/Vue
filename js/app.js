0196bb04b0e1a8ec99032644a3288c1b418831f5
guomin <yushi5344@gmail.com>
2018-04-26 20:57:22 +0800

    属性绑定

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ new Vue({
     el:"#vue-app",
     data:{
         name:"guomin",
-        job:"web开发"
+        job:"web开发",
+        website:"http://www.baidu.com",
+        websiteTag:"<a href='http://www.baidu.com'>百度</a>"
     },
     methods:{
         greet:function (time) {
@@ -14,5 +16,7 @@ new Vue({
 /*
 el:element 需要获取的元素，一定是html中的根容器元素
 data:用于数据的存储
+method:用于存储各种方法
+data-binding:给属性绑定对应的值
  */
 
