ec60b2f5dcecd409596731f23ce82b360afb555f
guomin <yushi5344@gmail.com>
2018-04-30 16:34:37 +0800

    钩子函数

--- myWebPack/my-project/src/components/footer.vue
+++ myWebPack/my-project/src/components/footer.vue
@@ -15,8 +15,33 @@
     },
     methods:{
       changeTitle:function () {
+        this.copyright='更新';
         this.$emit("titltChanged","子像父组件传值");
       }
+    },
+    beforeCreate:function () {
+      alert('组件实例化之前执行的函数');
+    },
+    created:function () {
+      alert('组件实例化完毕，但页面未显示');
+    },
+    beforeMount:function () {
+      alert('组件挂载之前，页面未展示，但虚拟DOM已经加载完毕');
+    },
+    mounted:function () {
+      alert('组件挂载后，此方法执行后，页面显示');
+    },
+    beforeUpdate:function () {
+      alert('组件更新前，页面未展示，但虚拟DOM已经位置完毕');
+    },
+    updated:function () {
+      alert('组件更新，此方法执行后，页面显示');
+    },
+    beforeDestroy:function () {
+      alert('组件销毁前');
+    },
+    Destroy:function () {
+      alert('组件已经销毁');
     }
   }
 </script>
